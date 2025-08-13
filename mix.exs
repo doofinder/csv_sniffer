53fa8454465ec60cd5bef6fd2765e7968f57705b
davidmolinacano <davidmolinacano@gmail.com>
2025-08-13 17:10:05 +0200

    feat: Pablo's latest changes

--- mix.exs
+++ mix.exs
@@ -28,9 +28,9 @@ defmodule CsvSniffer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.38", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.38.2", only: :dev, runtime: false}
     ]
   end
 
