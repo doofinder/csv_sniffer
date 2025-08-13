ba0599c3b2d071cf7b42dc7e95a8e5f691238e37
davidmolinacano <davidmolinacano@gmail.com>
2025-08-13 17:17:28 +0200

    feat: Add sobelow to deps

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule CsvSniffer.MixProject do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.38.2", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.38.2", only: :dev, runtime: false},
+      {:sobelow, "~> 0.14", only: [:dev, :test], runtime: false}
     ]
   end
 
